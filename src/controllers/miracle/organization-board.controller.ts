509884b62adcf14ebc4e150e69c790c9a02462fa
Sai Akhil Katukam <saiakhilk.katukam@gmail.com>
2024-07-10 02:27:47 +0530

    sort changes

--- src/controllers/miracle/organization-board.controller.ts
+++ src/controllers/miracle/organization-board.controller.ts
@@ -3,6 +3,7 @@ import sendResponse from '../../middlewares/send-response';
 import { MiracleBoard } from '../../models/miracle/board.model';
 import { ClerkUser } from '../../models/miracle/clerk-user.model';
 import { ClerkOrganization } from '../../models/miracle/clerk-organization.model';
+import { Types } from 'mongoose';
 
 @Controller('api/clerk/miracle-organization')
 export class MiracleOrganizationBoardController {
@@ -13,7 +14,7 @@ export class MiracleOrganizationBoardController {
             const { organizationId } = req.query;
             const boards = await MiracleBoard.find({
                 organizationIdClerk: organizationId,
-            }).sort('updatedAt').lean();
+            }).sort({ createdAt: -1 }).lean();
             return sendResponse(res, true, 'Success', boards);
         } catch (error) {
             return sendResponse(res, false, error.message, error);
@@ -53,6 +54,28 @@ export class MiracleOrganizationBoardController {
                 authorName: `${firstName} ${lastName}`,
                 imageUrl,
             })
+            return sendResponse(res, true, 'Success', true);
+        } catch (error) {
+            return sendResponse(res, false, error.message, error);
+        }
+    }
+
+    @Post('rename-board')
+    async renameBoard(req, res) {
+        try {
+            const { _id, title } = req.query;
+            await MiracleBoard.findOneAndUpdate({ _id: new Types.ObjectId(_id) }, { $set: { title } })
+            return sendResponse(res, true, 'Success', true);
+        } catch (error) {
+            return sendResponse(res, false, error.message, error);
+        }
+    }
+
+    @Post('delete-board')
+    async deleteBoard(req, res) {
+        try {
+            const { boardId } = req.query;
+            await MiracleBoard.findOneAndDelete({ _id: new Types.ObjectId(boardId) })
             return sendResponse(res, true, 'Success', true);
         } catch (error) {
             return sendResponse(res, false, error.message, error);
