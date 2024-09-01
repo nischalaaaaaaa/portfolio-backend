a86a71f398bf623f03f006e737c1b42be2b07329
Sai Akhil Katukam <saiakhilk.katukam@gmail.com>
2024-09-01 13:14:57 +0530

    export

--- ecosystem-config.js
+++ ecosystem-config.js
@@ -1,9 +1,9 @@
 module.exports = {
     apps: [{
         name: "app",
-        script: "dist/server.js",
+        script: "export NODE_ENV=prod && dist/server.js",
         instances: 0,
         exec_mode: "cluster",
-        node_args: "export NODE_ENV=prod --max-old-space-size=5120 -r dotenv/config",
+        node_args: "--max-old-space-size=5120 -r dotenv/config",
     }]
 }
